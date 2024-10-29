f34fdcb60609461822eaf732238fc1ae6fda3585
Robert Ardeleanu <synctax23@gmail.com>
2024-10-29 09:55:38 +0100

    Aufgabe 3 angepasst

--- src/ue02_3.java
+++ src/ue02_3.java
@@ -3,19 +3,22 @@ import libraries.In;
 
 public class ue02_3 {
     public static void main(String[] args) {
+        double radius = 4;
+
         Out.println("Geben Sie die x-Koordinate an:");
         double x = In.readDouble();
-
         Out.println("Geben Sie die y-Koordinate an:");
         double y = In.readDouble();
 
-        double radius = 4;
         double hypotenuse = Math.sqrt(x * x + y * y);
 
-        if (hypotenuse > radius) {
-            Out.println("Die Hypotenuse beträgt " + hypotenuse + ". Der Punkt liegt also innerhalb der grünen Fläche!");
+        if (isInBound(x, radius) && isInBound(y, radius) && hypotenuse > radius) {
+            Out.println("Der Punkt liegt innerhalb der grünen Fläche!");
         } else {
-            Out.println("Die Hypotenuse beträgt " + hypotenuse + "Der Punkt liegt also innerhalb des Kreises!");
+            Out.println("Der Punkt liegt NICHT innerhalb der grünen Fläche!");
         }
+    }
+    public  static  boolean isInBound(double number, double radius) {
+        return number > 0 && number <= radius;
     }
 }
